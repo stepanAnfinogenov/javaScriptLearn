f7f7efdf93db2434fb4e952af4e90e63018fdfcb
stepan <stepan.anfinogenov@gmail.com>
2023-02-09 18:57:42 +0000

    initial commit

---
+++ script.js
@@ -0,0 +1,36 @@
+'use strict';
+
+
+var leftBorderWith = 1;
+let second = 2;
+const pi = 3.14;
+
+console.log(leftBorderWith);
+console.log(4/0);
+console.log('string'*9);
+let leftBorderWi;
+console.log(leftBorderWi);
+
+// console.log(leftBorder);
+
+let persone = {
+    name: "Jone",
+    age: 25,
+    isMarried: false
+}
+
+console.log(persone.name);
+console.log(persone ["name"]);
+
+let arr = ['plum.png', 'orange.jpg', 'apple.bmp'];
+console.log(arr[0]);
+
+// alert("Hello!");
+
+// let answer = confirm("Are you here?")
+// console.log(answer);
+
+
+let answer = prompt("Are you 18+ ?", "Yes")
+console.log(answer);
+console.log(typeof(answer));
