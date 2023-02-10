abb36cc61608cb5beadd7ac372dcac9575cf5271
stepan <stepan.anfinogenov@gmail.com>
2023-02-10 13:49:29 +0000

    2.7 hw

--- script.js
+++ script.js
@@ -1,36 +1,51 @@
 'use strict';
 
+// let money = + prompt("write down necessary point of expenses this month")
+// let time = prompt("How match will it cost?");
 
-var leftBorderWith = 1;
-let second = 2;
-const pi = 3.14;
+// let expenses = {
+//     money,
+//     time
+// }
 
-console.log(leftBorderWith);
-console.log(4/0);
-console.log('string'*9);
-let leftBorderWi;
-console.log(leftBorderWi);
+// let appData = {
+//     money,
+//     time,
+//     expenses,
+//     optionalExpenses: 0,
+//     incom: [],
+//     savings: false
+// }
 
-// console.log(leftBorder);
+// console.log(money);
+// console.log(typeof(money))
+// console.log(time);
+// console.log(expenses);
+// console.log(appData);
 
-let persone = {
-    name: "Jone",
-    age: 25,
-    isMarried: false
-}
+// let dayBuget = money/30;
 
-console.log(persone.name);
-console.log(persone ["name"]);
-
-let arr = ['plum.png', 'orange.jpg', 'apple.bmp'];
-console.log(arr[0]);
-
-// alert("Hello!");
-
-// let answer = confirm("Are you here?")
-// console.log(answer);
+// alert('One day buget is: ' + dayBuget);
 
 
-let answer = prompt("Are you 18+ ?", "Yes")
-console.log(answer);
-console.log(typeof(answer));
+let money = prompt("Ваш бюджет на месяц?", ''),
+	time = prompt('Введите дату в формате YYYY-MM-DD', '');
+
+let appData = {
+	budget: money,
+	expenses: {},
+	optionalExpenses: {},
+	income: [],
+	timeData: time,
+	savings: false
+};
+
+let a1 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+	a2 = prompt("Во сколько обойдется?", ''),
+	a3 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+	a4 = prompt("Во сколько обойдется?", '');
+
+appData.expenses.a1 = a2;
+appData.expenses.a3 = a4;
+
+alert(appData.budget / 30);
